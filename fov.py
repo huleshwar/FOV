20d0308831a335bee52f6f8173f68ef525df5134
huleshwar chandra <hulesh@MacBook-Pro-3.local>
2016-05-01 04:10:16 +0530

    Added scripts for writing to mysql database

--- fov.py
+++ fov.py
@@ -13,4 +13,12 @@ taxRevenue.drop(taxRevenue.columns[[-1, 0, 2, 3]], axis=1, inplace=True)
 health = pd.read_excel("health/API_SH.XPD.TOTL.ZS_DS2_en_v2.xls", skiprows=3)
 health.drop(health.columns[[-1, 0, 2, 3]], axis=1, inplace=True)
 
+education.rename(columns = {"Country Code": "country_code"}, inplace = True)
+health.rename(columns = {"Country Code": "country_code"}, inplace=True)
+taxRevenue.rename(columns = {"Country Code": "country_code"}, inplace=True)
+cashSurplusDeficit.rename(columns = {"Country Code": "country_code"}, inplace=True)
 
+education.to_sql(name='education', con=cnx, if_exists = 'append', index=False, flavor="mysql")
+cashSurplusDeficit.to_sql(name='cashSurplusDeficit', con=cnx, if_exists = 'append', index=False, flavor="mysql")
+health.to_sql(name='health', con=cnx, if_exists = 'append', index=False, flavor="mysql")
+taxRevenue.to_sql(name='taxRevenue', con=cnx, if_exists = 'append', index=False, flavor="mysql")
